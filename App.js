21a22a856902d77b125345590d265ae340cb750e
Samuel Cursino <111611349+samuelcursino@users.noreply.github.com>
2023-03-09 21:25:07 -0300

    21:25

--- App.js
+++ App.js
@@ -6,10 +6,30 @@ import {View, Text, TextInput, ScrollView, Image, TouchableOpacity, Alert }
 const App = () => {
   const [texto, setTexto] = useState('Título Inicial');
   const [clicou, setClicou] = useState(false);
+  const [entrada, setEntrada] = useState('');
+
+  // useEffect(()=>{
+  //   alert('sempre dispara na renderização')
+  // })
+  
+  useEffect(()=>{
+     alert('mensagem inicial')
+    }, [])
+    
+  useEffect(()=>{
+    if(clicou == true) 
+    alert('Botão foi clidado e foi escrito: ' + (entrada))
+    return ()=>{
+    setClicou(false)
+    }
+   }, [clicou])
+   
 return (
         <View style={estilo.container}>
             <Text style={estilo.texto}>{texto}</Text>
-            <TextInput style={estilo.caixa}/>
+            <TextInput style={estilo.caixa}
+            value={entrada}
+            onChangeText={(e)=>setEntrada(e)} />
             <TouchableOpacity style={estilo.botao}
               onPress={()=>{
               setClicou(true)
