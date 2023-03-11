916efe873e50afd09a0e5dc5de933673cf3ad9d2
Samuel Cursino <111611349+samuelcursino@users.noreply.github.com>
2023-03-10 21:37:11 -0300

    atualização

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React, {useEffect, useState } from 'react'
-import {View, Text, TextInput, ScrollView, Image, TouchableOpacity, Alert }
- from 'react-native' 
- import estilo from './Estilo'
- import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import {
+  View, Text, TextInput, ScrollView,
+  Image, TouchableOpacity, Alert, FlatList, VirtualizedList
+}
+  from 'react-native'
+import estilo from './Estilo'
+import axios from 'axios'
  
 const App = () => {
-  const [texto, setTexto] = useState('Título Inicial');
+  const [texto, setTexto] = useState([]);
   const [clicou, setClicou] = useState(false);
   const [entrada, setEntrada] = useState('');
 
@@ -14,18 +17,18 @@ const App = () => {
   // })
   
   const pesquisa = () => {
-    axios('https://www.omdbapi.com/?apikey=d8a44ab&type=movie&r=json&page=1&s=love')
-    .then(function (response){
-      setTexto(JSON.stringify(response.data.Search))
+    axios.get('https://www.omdbapi.com/?apikey=d8a44ab&type=movie&r=json&page=1&s=love')
+    .then((response)=>{
+      setTexto(response.data)
     })
     .catch((error)=> {
       console.log(error);
-    })
+    });
   }
 
-  useEffect(()=>{
-     alert('mensagem inicial')
-    }, [])
+  // useEffect(()=>{
+  //    alert('mensagem inicial')
+  //   }, [])
     
   useEffect(()=>{
     // if(clicou == true) 
@@ -42,23 +45,25 @@ const App = () => {
 return (
   <ScrollView>
         <View style={estilo.container}>
-            <Text style={estilo.texto}>{texto}</Text>
+        <Text style={estilo.texto}>{JSON.stringify(texto.Search)}</Text>
+
             <TextInput style={estilo.caixa}
             value={entrada}
             onChangeText={(e)=>setEntrada(e)} />
+
             <TouchableOpacity style={estilo.botao}
               onPress={()=>{
               setClicou(true)
               // if(clicou == true) setClicou(false)
               // Alert.alert('Botão Clicado ' + (clicou))
-            }}
-            >
+            }}>
+
               <Text style={estilo.textoBotao}>BUSCAR DADOS</Text>
               </TouchableOpacity>
 
               <FlatList
-              data={texto}
-              render={({item})=><Text>{item.Title}</Text>}
+              data={texto.Search}
+              renderItem={({item})=><Text style={estilo.texto}>{item.Title}</Text>}
               />
 
         </View>
